3ecfea6a76de47fa1ef8d42be08043bb6073d72c
Louk-Chemdoc <lucasmessina83@hotmail.com>
2025-11-27 15:24:20 +0100

    tranformation image focntionelle

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    path('api/unet-transform/', views.segment_image_view, name='unet_transform'),
 ]
